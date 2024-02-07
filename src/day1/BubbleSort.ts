0fcc92a12a08beb301bff34466fe3fd27c8772e0
sawyer1776 <blackcreekproductions@gmail.com>
2024-02-07 09:47:20 -0500

    test

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,12 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let r = 0; r < arr.length; r++) {
+        for (let i = 0; i < arr.length - r - 1; i++) {
+            const curr = arr[i];
+            const next = arr[i + 1];
+            if (curr > next) {
+                arr[i] = next;
+                arr[i + 1] = curr;
+            }
+        }
+    }
 }
